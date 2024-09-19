12dbf041c8275dfbb8bd1d0bc0f31036096232a0
Okera Johnson <okerajohnson@gmail.com>
2024-09-19 12:20:45 -0400

    f the storage

--- components/QuoteScreen.js
+++ components/QuoteScreen.js
@@ -32,19 +32,10 @@ export default class QuoteScreen extends Component {
     return (
       <View style={styles.container}>
 
-<Text style={styles.label}>Enter your input here:</Text>
+
         
-        <TextInput
-          style={styles.inputField}
-          value={input}
-          placeholder="Enter"
-          onChangeText={onChangeText}
-          onSubmitEditing={onSubmitEditing}
-        />
-        <Text style={styles.text}>Your input is {input}</Text>
-        <Pressable onPress={clearStorage} style={styles.button}>
-          <Text style={styles.buttonText}>Clear Storage</Text>
-        </Pressable>
+       
+        
         <Text>{this.state.quote.content}</Text>
         <Text style={styles.alignRight}>
           {this.state.quote.author ? " - " + this.state.quote.author : ""}
@@ -72,55 +63,5 @@ export default class QuoteScreen extends Component {
     }
   };
 }
-
-
-let STORAGE_KEY = '@user_input';
-
-  const [input,setInput] = useState('');
-
-
-const saveData = async () => {
-  try {
-      awaitAsyncStorage.setItem(STORAGE_KEY, age)
-      alert('Data successfully saved')
-  } catch (e) {
-      alert ('Failed to save data')
-  }
-
-const readData = async () => {
-  try {
-      const value = await AsyncStorage.getItem(STORAGE_KEY);
-
-      if (value !== null) {
-          setInput(value);
-      }
-  } catch (e) {
-      alert ('failed to fetch the input from storage')
-  }
-}
-}
-
-const clearStorage = async () => {
-  try {
-      await AsyncStorage.clear();
-      alert('Storage succesffully cleared');
-  } catch (e) {
-      alert('Failed to clear the async storage');
-  }
-};
-
-const onChangeText = value => setInput(value);
-
-const onSubmitEditing = () => {
-  if (!input) return;
-  
-  saveData(input);
-  setInput('');
-};
-useEffect(() => {
-  readData();
-}, []);
-
-
 
 
